ebf5cbdb684486c1dbf71db73fdff26aa9b717d5
ruwaidObeidSeera <ruwaid.obeid@seera.sa>
2022-10-17 17:59:10 +0300

    feat: add snackbar and onClick event

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
+
 import Card from "../Components/Card/Card";
+import SnackBar from "../Components/SnackBar/SnackBar";
 import YouMightLike from "../Components/YouMightLike/YouMightLike";
 import styles from "./Home.module.scss";
 import data from "../data.json";
+
+const snackBarData = 'Welcome to the App'
 export default function Home() {
+  const [isSnackBarVisible, setIsSnackBarVisible] = useState(false)
+
+  const showSnackBar = () => {
+    setIsSnackBarVisible(true)
+  }
+
+  const onItemClick = () => {
+    showSnackBar()
+  }
+
   return (
     <div className={styles.Home}>
       <div className={styles.Container}>
         <Card title="Summer Sale" />
-        <YouMightLike title="You might also like" data={data} />
+        <YouMightLike data-testId="youMightLikeWrapper" title="You might also like" data={data} onItemClick={onItemClick} />
         <YouMightLike title="Top sellers" data={data} />
         <Card title="Limitied Time Only" var="var1" />
         <YouMightLike title="Near You" data={data} />
       </div>
+      <SnackBar 
+        open={isSnackBarVisible} 
+        data={snackBarData} 
+        onClose={()=> setIsSnackBarVisible(false)}
+      />
     </div>
   );
 }
